b0dd61bd6733f7e55f5e9200a1be29c67e82f303
Jędrzej Kłyza <neshernos@gmail.com>
2022-12-03 21:34:52 +0100

    fixed a bug with distance function

--- MusicGenreClasisfier.py
+++ MusicGenreClasisfier.py
@@ -9,10 +9,12 @@ import operator
 import math
 import numpy as np
 
+import ModelTest
+
 def getNeighbors(trainingSet, instance, k):
     distances = []
     for x in range (len(trainingSet)):
-        dist = distance(trainingSet[x], instance, k )+ distance(instance, trainingSet[x], k)
+        dist = ModelTest.distance(trainingSet[x], instance, k )+ ModelTest.distance(instance, trainingSet[x], k)
         distances.append((trainingSet[x][2], dist))
     distances.sort(key=operator.itemgetter(1))
     neighbors = []
